a5749635f152893d87a8710432166239519adc13
xztaityozx <taityonohibi@gmail.com>
2019-09-10 16:11:55 +0900

    update test

---
+++ tools/Ptolemy.Argo/Options.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace Ptolemy.Argo {
+    public class Options {
+
+        public static Options Parse(IEnumerable<string> args) {
+            Options rt = null;
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o => rt = o)
+                .WithNotParsed(e => throw new ArgoParseFailedException());
+            return rt ?? throw new NullReferenceException();
+        }
+        
+        public const string TimeDefault = "0,100p,20n", VtnDefault = "0.6,0.046,1.0", VtpDefault = "-0.6,0.046,1.0";
+        private const string NonJson = "nonJson", Json = "Json";
+        [Option("time",Default = TimeDefault, SetName = NonJson, HelpText = "シミュレーション時間を指定します。何も書かない場合はデフォルト値が適応されます [start],[step],[stop]")]
+        public string TimeString { get; set; }
+        
+        [Option('b', "base", SetName = NonJson, Default = "./", HelpText = "結果を出力するディレクトリのルートです")]
+        public string BaseDir { get; set; }
+        
+        [Option('r', "root", HelpText = "回路が格納されているディレクトリ、例えば~/simulationなどへのパスです。指定しない場合、環境変数 `ARGO_CIRCUIT_ROOT` が適用されます")]
+        public string CircuitRoot { get; set; }
+        
+        [Option('h', "hspice", SetName = NonJson, HelpText = "hspiceへのパスです。指定しない場合、環境変数 `ARGO_HSPICE` が適用されます")]
+        public string Hspice { get; set; }
+        
+        [Option("options", SetName = NonJson, HelpText = "hspiceへ渡すオプションです")]
+        public IEnumerable<string> HspiceOptions { get; set; }
+        
+        [Option('N', "vtn", Default = VtnDefault, SetName = NonJson,
+            HelpText = "vtnの値をカンマ区切りで指定します。何も書かない場合はデフォルト値が適用されます [threshold],[sigma],[deviation]")]
+        public string VtnString { get; set; }
+
+        [Option('P', "vtp", Default = VtpDefault, SetName = NonJson,
+            HelpText = "vtpの値をカンマ区切りで指定します。何も書かない場合はデフォルト値が適用されます [threshold],[sigma],[deviation]")]
+        public string VtpString { get; set; }
+
+        [Option('T',"temperature", Default = 25.0, HelpText = "温度です", SetName = NonJson)]
+        public double Temperature { get; set; }
+        
+        [Option('t', "target", SetName = NonJson,  HelpText = "シミュレーションしたい回路の名前を /username/cell/circuit で指定します")]
+        public string TargetCircuit { get; set; }
+        
+        [Option('w', "sweeps",Default = 5000L, HelpText = "Sweep数です", SetName = NonJson)]
+        public long Sweeps { get; set; }
+        
+        [Option('e', "seed", SetName = NonJson, Default = 1L, HelpText = "Seed値です")]
+        public long Seed { get; set; }
+        
+        [Option('d', "vdd", Default = 0.8, HelpText = "電源電圧(V)です", SetName = NonJson)]
+        public double Vdd { get; set; }
+        
+        [Option('g', "gnd", Default = 0.0, HelpText = "Gndの電圧(V)です", SetName = NonJson)]
+        public double Gnd { get; set; }
+        
+        [Option('c', "icCommand", Default = new[]{"V(N1)=0.8V","V(N2)=0V"}, Separator = ',',SetName = NonJson,HelpText = "SPIスクリプトでの .IC コマンド渡す値をカンマ区切りで与えます")]
+        public IEnumerable<string> IcCommands { get; set; }
+        
+        [Option('m',"model", SetName = NonJson, HelpText = "modelファイルへのパスです")]
+        public string ModelFile { get; set; }
+        
+        [Option("sweepStart", Default = 1L, HelpText = "Sweepの開始値です", SetName = NonJson)]
+        public long SweepStart { get; set; }
+        
+        [Option("generateJson", SetName = NonJson, Default = false, HelpText = "指定されたパラメータをもとにJSONを作成し、stdoutに出力します")]
+        public bool GenerateJson { get; set; }
+        
+        [Option("json", SetName = Json, HelpText = "シミュレーションを記述したJSONをもとに実行します")]
+        public string JsonFile { get; set; }
+    }
+}
--- tools/Ptolemy.Argo/Program.cs
+++ tools/Ptolemy.Argo/Program.cs
@@ -1,81 +1,116 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using CommandLine;
+using System.Linq;
 using CommandLine.Text;
 using Ptolemy.Argo.Request;
 using Ptolemy.Parameters;
+using YamlDotNet.Core.Events;
 
 namespace Ptolemy.Argo {
     internal static class Program {
         private static void Main(string[] args) {
-            
-            // TODO: ここやろうね
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(o => {
-                    ArgoRequest request = null;
-                    if (string.IsNullOrEmpty(o.JsonFile)) {
-                        if (!File.Exists(o.JsonFile))
-                            throw new FileNotFoundException("json file not found", o.JsonFile);
-                        using (var sr = new StreamReader(o.JsonFile)) request = ArgoRequest.FromJson(sr.ReadToEnd());
-                    }
-                    else {
-                        request = new ArgoRequest {
-                            TargetCircuit = o.TargetCircuit,
-                            Sweep = o.Sweeps,
-                            Gnd = (decimal)o.Gnd,
-                            Seed = o.Seed,
-                            Temperature = (decimal)o.Temperature,
-                            Vdd = (decimal)o.Vdd,
-                            
-                        };
-                    }
-                });
+            try {
+                var log = new Logger.Logger();
+                Options.Parse(args);
+            }
+            catch (ArgoParseFailedException) {
+                Console.WriteLine();
+            }
         }
     }
 
-    public class Options {
-        private const string NonJson = "nonJson", Json = "Json";
-        [Option('N', "vtn", Default = "0.6,0.046,1.0", SetName = NonJson,
-            HelpText = "vtnの値をカンマ区切りで指定します。何も書かない場合はデフォルト値が適用されます [threshold],[sigma],[deviation]")]
-        public string VtnString { get; set; }
+    public class Argo {
+        private readonly ArgoRequest request;
+        private readonly Logger.Logger log;
 
-        [Option('P', "vtp", Default = "-0.6,0.046,1.0", SetName = NonJson,
-            HelpText = "vtpの値をカンマ区切りで指定します。何も書かない場合はデフォルト値が適用されます [threshold],[sigma],[deviation]")]
-        public string VtpString { get; set; }
+        public Argo(Options o, Logger.Logger log) {
+            this.log = log;
 
-        [Option('T',"temperature", Default = 25.0, HelpText = "温度です", SetName = NonJson)]
-        public double Temperature { get; set; }
-        
-        [Option('t', "target", SetName = NonJson, Required = true, HelpText = "シミュレーションしたい回路の名前を /username/cell/circuit で指定します")]
-        public string TargetCircuit { get; set; }
-        
-        [Option('w', "sweeps",Default = 5000L, HelpText = "Sweep数です", SetName = NonJson)]
-        public long Sweeps { get; set; }
-        
-        [Option('e', "seed", SetName = NonJson, Default = 1L, HelpText = "Seed値です")]
-        public long Seed { get; set; }
-        
-        [Option('d', "vdd", Default = 0.8, HelpText = "電源電圧(V)です", SetName = NonJson)]
-        public double Vdd { get; set; }
-        
-        [Option('g', "gnd", Default = 0.0, HelpText = "Gndの電圧(V)です", SetName = NonJson)]
-        public double Gnd { get; set; }
-        
-        [Option('c', "icCommand", Default = new[]{"V(N1)=0.8V","V(N2)=0V"}, Separator = ',',SetName = NonJson,HelpText = "SPIスクリプトでの .IC コマンド渡す値をカンマ区切りで与えます")]
-        public IEnumerable<string> IcCommands { get; set; }
-        
-        [Option('m',"model",Required = true, SetName = NonJson, HelpText = "modelファイルへのパスです")]
-        public string ModelFile { get; set; }
-        
-        [Option("sweepStart", Default = 1L, HelpText = "Sweepの開始値です", SetName = NonJson)]
-        public long SweepStart { get; set; }
-        
-        [Option("generateJson", SetName = NonJson, Default = false, HelpText = "指定されたパラメータをもとにJSONを作成し、stdoutに出力します")]
-        public bool GenerateJson { get; set; }
-        
-        [Option("json", SetName = Json)]
-        public string JsonFile { get; set; }
+            o.CircuitRoot = o.CircuitRoot ?? Environment.GetEnvironmentVariable("ARGO_CIRCUIT_ROOT");
+            o.Hspice = o.Hspice ?? Environment.GetEnvironmentVariable("ARGO_HSPICE");
+            o.TargetCircuit = o.TargetCircuit ?? Environment.GetEnvironmentVariable("ARGO_TARGET_CIRCUIT");
+            o.ModelFile = o.ModelFile ?? Environment.GetEnvironmentVariable("ARGO_MODEL_FILE");
+
+            if (string.IsNullOrEmpty(o.CircuitRoot)) {
+                throw new ArgoException(
+                    "circuit root is not set. please check env:ARGO_CIRCUIT_ROOT or -r,--root option");
+            }
+
+            if (string.IsNullOrEmpty(o.Hspice)) {
+                throw new ArgoException(
+                    $"hspice is not set. please check env:ARGO_HSPICE or -h,--hspice option");
+            }
+
+            if (string.IsNullOrEmpty(o.TargetCircuit)) {
+                throw new ArgoException(
+                    $"target circuit is not set. please check env:ARGO_TARGET_CIRCUIT or -t,--target option");
+            }
+
+            if (string.IsNullOrEmpty(o.ModelFile)) {
+                throw new ArgoException(
+                    $"model file is not set. please check env:ARGO_MODEL_FILE or -m,--model option");
+            }
+            
+
+            log.Info($"circuit root: {o.CircuitRoot}");
+            if (!string.IsNullOrEmpty(o.JsonFile)) {
+                log.Info($"use json request: file--> {o.JsonFile}");
+                if (!File.Exists(o.JsonFile)) {
+                    throw new ArgoException($"Ptolemy.Argo cannot find {o.JsonFile}");
+                }
+
+                try {
+                    using (var sr = new StreamReader(o.JsonFile))
+                        request = ArgoRequest.FromJson(sr.ReadToEnd());
+                }
+                catch (Exception e) {
+                    throw new ArgoException($"Failed to parse json to ArgoRequest\n\tinnerException-->{e}");
+                }
+
+                log.Info($"Success parse json to request\n{request}");
+            }
+            else {
+                (decimal, decimal, decimal) Bind(string def, string input) {
+                    var box = input.Split(',')
+                        .Zip(def.Split(','), (i, d) => string.IsNullOrEmpty(i) ? d : i)
+                        .Select(SiMetricPrefix.SiMetricPrefix.ParseDecimalWithSiPrefix)
+                        .ToList();
+                    return (box[0], box[1], box[2]);
+                }
+
+                log.Info("Building request from cli options");
+
+                try {
+                    request = new ArgoRequest {
+                        TargetCircuit = o.TargetCircuit,
+                        Sweep = o.Sweeps,
+                        Gnd = (decimal) o.Gnd,
+                        Seed = o.Seed,
+                        Temperature = (decimal) o.Temperature,
+                        Vdd = (decimal) o.Vdd,
+                        IcCommands = o.IcCommands.ToList(),
+                        SweepStart = o.SweepStart,
+                        ModelFilePath = o.ModelFile,
+                        BaseDirectory = o.BaseDir,
+                        HspiceOptions = o.HspiceOptions.ToList(),
+                        Time = new Range(Bind(Options.TimeDefault, o.TimeString)),
+                        Vtn = new Transistor(Bind(Options.VtnDefault, o.VtnString)),
+                        Vtp = new Transistor(Bind(Options.VtpDefault, o.VtpString)),
+                        HspicePath = o.Hspice ?? Environment.GetEnvironmentVariable("ARGO_HSPICE"),
+                        GroupId = Guid.NewGuid()
+                    };
+                }
+                catch (Exception e) {
+                    throw new ArgoException(
+                        $"Failed to build request from cli options\n\tinnerException-->{e}");
+                }
+
+                log.Info($"Success parse request:{request}");
+
+            }
+        }
     }
+
+    public class ArgoParseFailedException:Exception {}
 }
