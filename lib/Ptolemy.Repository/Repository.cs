105c24ed027054b3bfec2f1ffd22b9ca37ec60e0
xztaityozx <taityonohibi@gmail.com>
2019-09-20 16:49:51 +0900

    DBわからんで〜〜〜〜

--- lib/Ptolemy.Repository/Repository.cs
+++ lib/Ptolemy.Repository/Repository.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ptolemy.Repository {
     public class Repository {
+        
+    }
+
+    internal class Context : DbContext {
+        public DbSet<ResultEntity> Entities { get; set; }
     }
 }
---
+++ lib/Ptolemy.Repository/ResultEntity.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Ptolemy.Map;
+using Ptolemy.SiMetricPrefix;
+
+namespace Ptolemy.Repository {
+    public class ResultEntity {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required] public decimal Time { get; set; }
+        [Required] public Map.Map<string, decimal> Values { get; set; }
+       
+        [Required] public long Seed { get; set; }
+        [Required] public long Sweep { get; set; }
+        
+        [NotMapped] public string DbName { get; set; }
+        
+        public ResultEntity(){}
+
+        public ResultEntity(string dbName, long seed, long sweep, string hspiceOutputLine, IEnumerable<string> keys) {
+            var split = hspiceOutputLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Time = split[0].ParseDecimalWithSiPrefix();
+            Values=new Map<string, decimal>();
+            foreach (var d in split.Skip(1).Zip(keys, (v,k) => new{v,k})) {
+                Values[d.k] = d.v.ParseDecimalWithSiPrefix();
+            }
+
+            Seed = seed;
+            Sweep = sweep;
+            DbName = dbName;
+        }
+    }
+
+    public class ParameterEntity {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required] public string Vtn { get; set; }
+        [Required] public string Vtp { get; set; }
+        [Required] public string NetListName { get; set; }
+        [Required] public string DatabaseName { get; set; }
+    }
+}
