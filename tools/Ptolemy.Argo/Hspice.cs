303ed8aba4e6b0ee26a2be9aa60d1d6abed6720f
xztaityozx <taityonohibi@gmail.com>
2019-10-17 18:47:32 +0900

    わからん

---
+++ tools/Ptolemy.Argo/Hspice.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Ptolemy.Argo.Request;
+using Ptolemy.Repository;
+using Ptolemy.SiMetricPrefix;
+
+namespace Ptolemy.Argo {
+    public class Hspice {
+        private readonly string hspice, workingRoot;
+
+        public Hspice(string hspicePath, string workingRoot) {
+            hspice = hspicePath;
+            this.workingRoot = workingRoot;
+        }
+
+        public List<ResultEntity> Run(CancellationToken token, ArgoRequest request) {
+            var rt = new List<ResultEntity>();
+
+            Directory.CreateDirectory(workingRoot);
+            Directory.SetCurrentDirectory(workingRoot);
+
+            using (var p = new Process {
+                StartInfo = new ProcessStartInfo {
+                    UseShellExecute = false,
+                    FileName = "bash",
+                    ArgumentList = {"-c", "~/TestDir/hspice.sh"},
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                },
+            }) {
+                p.Start();
+                var stdout = p.StandardOutput;
+
+                var stderr = p.StandardError;
+
+                var signals = request.Signals;
+
+                var kind = HspiceOutKind.Else;
+                var sweep = request.SweepStart;
+                string line;
+                while ((line = stdout.ReadLine()) != null || !token.IsCancellationRequested && !p.HasExited) {
+
+                    if (string.IsNullOrEmpty(line)) continue;
+                    if (line[0] == 'x') {
+                        kind = HspiceOutKind.Data;
+                    } 
+                    else if (line[0] == 'y') {
+                        sweep++;
+                        kind = HspiceOutKind.Else;
+                    }
+
+                    if (kind == HspiceOutKind.Else) continue;
+                    var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    if (!split[0].TryParseDecimalWithSiPrefix(out _)) continue;
+
+                    Console.WriteLine(line);
+
+                    //rt.AddRange(ResultEntity.Parse(request.Seed, sweep, line, signals));
+                }
+
+                p.WaitForExit();
+            }
+
+            return rt;
+        }
+
+        private enum HspiceOutKind {
+            Data,Else
+        }
+    }
+}
--- tools/Ptolemy.Argo/Program.cs
+++ tools/Ptolemy.Argo/Program.cs
@@ -7,119 +7,133 @@ using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Text;
+using System.Text.Json;
 using System.Threading;
 using CommandLine;
 using Kurukuru;
+using Ptolemy.Argo.Request;
 using ShellProgressBar;
 
 namespace Ptolemy.Argo {
     internal static class Program {
         private static void Main(string[] args) {
-            var log = new Logger.Logger();
-            log.Info("Welcome to Ptolemy.Argo");
-            try {
-                var req = Parser.Default.ParseArguments<ArgoOption>(args)
-                    .MapResult(o => {
-                        if (!o.Clean) return o.BuildRequest();
-                        var path = Path.Combine(Path.GetTempPath(), "Ptolemy.Argo");
-                        Spinner.Start("Cleanup...", spin => {
-                            if (Directory.Exists(path)) Directory.Delete(path, true);
-                            spin.Info("Finished");
-                        });
-                        
+            var h = new Hspice("", FilePath.FilePath.DotConfig);
+            var rt = h.Run(CancellationToken.None, new ArgoRequest {
+                Signals = new List<string>{"n1","n2","blb","bl"},
+                SweepStart = 1
+            });
 
-                        throw new ArgoClean();
-                    }, e => throw new ArgoParseFailedException());
-                var results = new StringBuilder();
-
-                Console.Clear();
-                log.Info($"Target Netlist: {req.NetList}");
-                log.Info("Parameters");
-                log.Info("\tVtn:");
-                log.Info($"\t\tThreshold: {req.Transistors.Vtn.Threshold}");
-                log.Info($"\t\tSigma: {req.Transistors.Vtn.Sigma}");
-                log.Info($"\t\tDeviation: {req.Transistors.Vtn.Deviation}");
-                log.Info("\tVtp:");
-                log.Info($"\t\tThreshold: {req.Transistors.Vtp.Threshold}");
-                log.Info($"\t\tSigma: {req.Transistors.Vtp.Sigma}");
-                log.Info($"\t\tDeviation: {req.Transistors.Vtp.Deviation}");
-                log.Info($"\tSweeps: {req.SweepStart:E} to {req.Sweep + req.SweepStart - 1:E}");
-                log.Info($"\tSeed: {req.Seed:E}");
-                log.Info($"\tTemperature: {req.Temperature}");
-                log.Info($"\tSimulationTime: {req.Time.Start:E} to {req.Time.Stop:E} (step={req.Time.Step:E})");
-                log.Info($"ExtractTargets: {string.Join(",", req.Signals)}");
-                log.Info($"Include {req.Includes.Count} files");
-                foreach (var inc in req.Includes) {
-                    log.Info($"\t--> {inc}");
-                }
-
-                log.Warn($"Press Ctrl+C to cancel");
-
-                using var cts = new CancellationTokenSource();
-                Console.CancelKeyPress += (sender, eventArgs) => {
-                    eventArgs.Cancel = true;
-                    cts.Cancel();
-                };
-                bool status;
-                var watch = new Stopwatch();
-                using var argo = new Argo(req, cts.Token);
-                using (var pb = new ProgressBar((int) req.Sweep, "Ptolemy.Argo", new ProgressBarOptions {
-                    BackgroundCharacter = '-',
-                    BackgroundColor = ConsoleColor.DarkGray,
-                    ForegroundColor = ConsoleColor.DarkBlue,
-                    ProgressCharacter = '>',
-                    ForegroundColorDone = ConsoleColor.Green,
-                    CollapseWhenFinished = false
-                })) {
-                    var sweep = req.SweepStart-1;
-                    var ob = argo.Receiver.Subscribe(s => {
-                        if (s[0] == 'x') {
-                            sweep++;
-                        }
-                        else {
-                            results.AppendLine(s);
-                        }
-                    });
-                    cts.Token.Register(ob.Dispose);
-                    watch.Start();
-                    (status, _) = argo.Run();
-                    watch.Start();
-                }
-
-                if (!status) {
-                    log.Error("Failed simulation");
-                }
-                else {
-                    log.Info("Finished simulation");
-                    log.Info($"Elapsed time: {watch.Elapsed}");
-                    log.Info("Result file: " + req.ResultFile +
-                             $"[{req.SweepStart}..{req.Sweep + req.SweepStart - 1}]");
-                    //foreach (var item in results.Select((sb, i) => new {sb, i = i + req.SweepStart})) {
-                    //    var path = req.ResultFile + $"{item.i}";
-                    //    using var sw = new StreamWriter(path, false, new UTF8Encoding(false));
-                    //    sw.WriteLine(item.sb.ToString().TrimEnd());
-                    //    sw.Flush();
-                    //}
-
-                    using var sw = new StreamWriter(req.ResultFile, false, Encoding.UTF8);
-                    sw.WriteLine(results.ToString());
-                    sw.Flush();
-                }
+            foreach (var resultEntity in rt) {
+                Console.WriteLine(JsonSerializer.Serialize(resultEntity));
             }
-            catch (ArgoClean) {
-                log.Info("Ptolemy.Argo clean temp directory /tmp/Ptolemy.Argo");
-            }
-            catch (ArgoParseFailedException) {
-                log.Warn("Failed parse options");
-            }
-            catch (ArgoException e) {
-                log.Error(e);
-                Environment.ExitCode = 1;
-            }
-            catch (Exception e) {
-                log.Error($"Unexpected exception was thrown\n-->{e}");
-                Environment.ExitCode = 1;
-            }
+
+            Console.WriteLine(rt.Count);
+            //    var log = new Logger.Logger();
+            //    log.Info("Welcome to Ptolemy.Argo");
+            //    try {
+            //        var req = Parser.Default.ParseArguments<ArgoOption>(args)
+            //            .MapResult(o => {
+            //                if (!o.Clean) return o.BuildRequest();
+            //                var path = Path.Combine(Path.GetTempPath(), "Ptolemy.Argo");
+            //                Spinner.Start("Cleanup...", spin => {
+            //                    if (Directory.Exists(path)) Directory.Delete(path, true);
+            //                    spin.Info("Finished");
+            //                });
+
+
+            //                throw new ArgoClean();
+            //            }, e => throw new ArgoParseFailedException());
+            //        var results = new StringBuilder();
+
+            //        Console.Clear();
+            //        log.Info($"Target Netlist: {req.NetList}");
+            //        log.Info("Parameters");
+            //        log.Info("\tVtn:");
+            //        log.Info($"\t\tThreshold: {req.Transistors.Vtn.Threshold}");
+            //        log.Info($"\t\tSigma: {req.Transistors.Vtn.Sigma}");
+            //        log.Info($"\t\tDeviation: {req.Transistors.Vtn.Deviation}");
+            //        log.Info("\tVtp:");
+            //        log.Info($"\t\tThreshold: {req.Transistors.Vtp.Threshold}");
+            //        log.Info($"\t\tSigma: {req.Transistors.Vtp.Sigma}");
+            //        log.Info($"\t\tDeviation: {req.Transistors.Vtp.Deviation}");
+            //        log.Info($"\tSweeps: {req.SweepStart:E} to {req.Sweep + req.SweepStart - 1:E}");
+            //        log.Info($"\tSeed: {req.Seed:E}");
+            //        log.Info($"\tTemperature: {req.Temperature}");
+            //        log.Info($"\tSimulationTime: {req.Time.Start:E} to {req.Time.Stop:E} (step={req.Time.Step:E})");
+            //        log.Info($"ExtractTargets: {string.Join(",", req.Signals)}");
+            //        log.Info($"Include {req.Includes.Count} files");
+            //        foreach (var inc in req.Includes) {
+            //            log.Info($"\t--> {inc}");
+            //        }
+
+            //        log.Warn($"Press Ctrl+C to cancel");
+
+            //        using var cts = new CancellationTokenSource();
+            //        Console.CancelKeyPress += (sender, eventArgs) => {
+            //            eventArgs.Cancel = true;
+            //            cts.Cancel();
+            //        };
+            //        bool status;
+            //        var watch = new Stopwatch();
+            //        using var argo = new Argo(req, cts.Token);
+            //        using (var pb = new ProgressBar((int) req.Sweep, "Ptolemy.Argo", new ProgressBarOptions {
+            //            BackgroundCharacter = '-',
+            //            BackgroundColor = ConsoleColor.DarkGray,
+            //            ForegroundColor = ConsoleColor.DarkBlue,
+            //            ProgressCharacter = '>',
+            //            ForegroundColorDone = ConsoleColor.Green,
+            //            CollapseWhenFinished = false
+            //        })) {
+            //            var sweep = req.SweepStart-1;
+            //            var ob = argo.Receiver.Subscribe(s => {
+            //                if (s[0] == 'x') {
+            //                    sweep++;
+            //                }
+            //                else {
+            //                    results.AppendLine(s);
+            //                }
+            //            });
+            //            cts.Token.Register(ob.Dispose);
+            //            watch.Start();
+            //            (status, _) = argo.Run();
+            //            watch.Start();
+            //        }
+
+            //        if (!status) {
+            //            log.Error("Failed simulation");
+            //        }
+            //        else {
+            //            log.Info("Finished simulation");
+            //            log.Info($"Elapsed time: {watch.Elapsed}");
+            //            log.Info("Result file: " + req.ResultFile +
+            //                     $"[{req.SweepStart}..{req.Sweep + req.SweepStart - 1}]");
+            //            //foreach (var item in results.Select((sb, i) => new {sb, i = i + req.SweepStart})) {
+            //            //    var path = req.ResultFile + $"{item.i}";
+            //            //    using var sw = new StreamWriter(path, false, new UTF8Encoding(false));
+            //            //    sw.WriteLine(item.sb.ToString().TrimEnd());
+            //            //    sw.Flush();
+            //            //}
+
+            //            using var sw = new StreamWriter(req.ResultFile, false, Encoding.UTF8);
+            //            sw.WriteLine(results.ToString());
+            //            sw.Flush();
+            //        }
+            //    }
+            //    catch (ArgoClean) {
+            //        log.Info("Ptolemy.Argo clean temp directory /tmp/Ptolemy.Argo");
+            //    }
+            //    catch (ArgoParseFailedException) {
+            //        log.Warn("Failed parse options");
+            //    }
+            //    catch (ArgoException e) {
+            //        log.Error(e);
+            //        Environment.ExitCode = 1;
+            //    }
+            //    catch (Exception e) {
+            //        log.Error($"Unexpected exception was thrown\n-->{e}");
+            //        Environment.ExitCode = 1;
+            //    }
+            //}
         }
     }
 }
