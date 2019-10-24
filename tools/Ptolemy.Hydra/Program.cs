782c9f802cbe3dd8bed0ae3d6abf151bbf8c0d1c
xztaityozx <taityonohibi@gmail.com>
2019-10-24 15:55:37 +0900

    branched tool/Hydra

--- tools/Ptolemy.Hydra/Program.cs
+++ tools/Ptolemy.Hydra/Program.cs
@@ -1,37 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using CancellationTokenSource = System.Threading.CancellationTokenSource;
 
-namespace Ptolemy.Hydra {
-    public class Program {
-        public static void Main(string[] args) {
-            using (var cts = new CancellationTokenSource()) {
-                var token = cts.Token;
-                var webApi = Task.Factory.StartNew(() =>
-                    CreateWebHostBuilder(args).Build().Run(), token);
-                var doTask = Task.Factory.StartNew(() => {
-                    while (!token.IsCancellationRequested) {
-                        Trace.WriteLine("Do");
-                        Thread.Sleep(1000);
-                    }
-                }, token);
-
-                Task.WaitAny(webApi, doTask);
-                cts.Cancel();
-            }
+namespace Ptolemy.Hydra
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
