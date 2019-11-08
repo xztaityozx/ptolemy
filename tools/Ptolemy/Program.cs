a0022116062e1a69de2abc8cd2e0bf3c58da1259
xztaityozx <taityonohibi@gmail.com>
2019-11-08 10:10:10 +0900

    update: PtolemyのMainをからっぽにした

--- tools/Ptolemy/Program.cs
+++ tools/Ptolemy/Program.cs
@@ -8,19 +8,6 @@ using Ptolemy.Repository;
 namespace Ptolemy {
     public static class Program {
         internal static void Main(string[] args) {
-            var pair = new[] {new {n = 0.6, p = -0.2}, new {n = 0.59, p = -0.2}};
-
-            foreach (var np in pair) {
-                var vtn = new Transistor(np.n, 0.046, 1.0);
-                var vtp = new Transistor(np.p, 0.046, 1.0);
-                var pe = new ParameterEntity {
-                    Vtn = vtn.ToString(), Vtp = vtp.ToString()
-                };
-
-                Console.WriteLine(vtn);
-                Console.WriteLine(vtp);
-                Console.WriteLine(pe.Hash());
-            }
         }
     }
 }
