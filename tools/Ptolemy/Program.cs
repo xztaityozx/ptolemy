f944b42681a63ce8be84a11cc444f07cfc2746a3
xztaityozx <taityonohibi@gmail.com>
2019-11-08 10:36:39 +0900

    Issue/31 (#32)
    * fixed: string.Joinに渡す引数がイカれてた
    
    * update: PtolemyのMainをからっぽにした

--- tools/Ptolemy/Program.cs
+++ tools/Ptolemy/Program.cs
@@ -2,13 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Ptolemy.Parameters;
+using Ptolemy.Repository;
 
 namespace Ptolemy {
     public static class Program {
         internal static void Main(string[] args) {
-            
         }
-
     }
-
 }
